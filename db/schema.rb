d6a6944ffd0abdad33ff734cec156e1f3ff5e282
ginahwang357 <vagrant@rails-dev-box>
2015-04-27 02:56:54 +0000

    add name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427024042) do
+ActiveRecord::Schema.define(version: 20150427025441) do
 
   create_table "blogs", force: true do |t|
     t.integer  "user_id"
