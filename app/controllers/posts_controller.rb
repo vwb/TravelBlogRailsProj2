fdd863ce3b8c6b805a37286afda9a7596f2c5abe
ginahwang357 <vagrant@rails-dev-box>
2015-04-29 07:08:31 +0000

    admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,30 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!
-
   def new
-	  @post = Post.new
+    @post = Post.new
   end
-
   def show
     @post = Post.find(params[:id])
   end
-
   def create
-	  @post = Post.create(post_params)
-    @post.user = current_user
-    @post.like = 0;
-	  if @post.save
-	    redirect_to root_path
-	  else
-	   render 'new'
-	  end
+    if current_user.try(:admin?) 
+     @post = Post.create(post_params)
+      @post.user = current_user
+      @post.like = 0;
+      if @post.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    else
+      redirect_to new_post_path
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
+   @post = Post.find(params[:id])
     if @post.destroy
-      redirect_to root_path
+     redirect_to root_path
     else
       :back
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428230240) do
+ActiveRecord::Schema.define(version: 20150429055754) do
 
   create_table "blogs", force: true do |t|
     t.integer  "user_id"
@@ -28,11 +28,6 @@ ActiveRecord::Schema.define(version: 20150428230240) do
     t.integer  "like"
   end
 
-  create_table "likes", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: true do |t|
     t.integer  "user_id"
     t.datetime "date"
@@ -46,12 +41,12 @@ ActiveRecord::Schema.define(version: 20150428230240) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -59,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150428230240) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
